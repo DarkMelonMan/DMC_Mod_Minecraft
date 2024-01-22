7f20038a8e00131eecb57491c74b5dc83eb2e0da
Егор Авцинов <esavtzinov@mail.ru>
2024-01-23 00:19:16 +0700

    Added Records with DMC OST

--- src/main/java/ru/unclestalin/devilmaycrymod/item/ModItems.java
+++ src/main/java/ru/unclestalin/devilmaycrymod/item/ModItems.java
@@ -1,17 +1,24 @@
 package ru.unclestalin.devilmaycrymod.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.RecordItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import ru.unclestalin.devilmaycrymod.DevilMayCryMod;
 import ru.unclestalin.devilmaycrymod.item.custom.VitalStar;
+import ru.unclestalin.devilmaycrymod.sound.ModSounds;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, DevilMayCryMod.MOD_ID);
     public static final RegistryObject<Item> RED_ORB = ITEMS.register("redorb", () -> new Item(new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
+    public static final RegistryObject<Item> BURY_THE_LIGHT_RECORD = ITEMS.register("bury_the_light_record", () -> new RecordItem(666, ModSounds.DMC5_VERGIL_THEME.get(), new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
+    public static final RegistryObject<Item> SUBHUMAN_RECORD = ITEMS.register("subhuman_record", () -> new RecordItem(666, ModSounds.DMC5_DANTE_THEME.get(), new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
+    public static final RegistryObject<Item> DEVIL_TRIGGER_RECORD = ITEMS.register("devil_trigger_record", () -> new RecordItem(666, ModSounds.DMC5_NERO_THEME.get(), new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
+    public static final RegistryObject<Item> CRIMSON_CLOUD_RECORD = ITEMS.register("crimson_cloud_record", () -> new RecordItem(666, ModSounds.DMC5_V_THEME.get(), new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
+    public static final RegistryObject<Item> DEVILS_NEVER_CRY_RECORD = ITEMS.register("devils_never_cry_record", () -> new RecordItem(666, ModSounds.DMC3_DEVILS_NEVER_CRY.get(), new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB)));
     public static final RegistryObject<Item> VITAL_STAR_S = ITEMS.register("vital_star_s", () -> new VitalStar(new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB).stacksTo(10), 5f));
     public static final RegistryObject<Item> VITAL_STAR_M = ITEMS.register("vital_star_m", () -> new VitalStar(new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB).stacksTo(10), 10f));
     public static final RegistryObject<Item> VITAL_STAR_L = ITEMS.register("vital_star_l", () -> new VitalStar(new Item.Properties().tab(DMCCreativeModeTab.DMC_TAB).stacksTo(10), 20f));
--- src/main/java/ru/unclestalin/devilmaycrymod/sound/ModSounds.java
+++ src/main/java/ru/unclestalin/devilmaycrymod/sound/ModSounds.java
@@ -12,6 +12,12 @@ public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, DevilMayCryMod.MOD_ID);
 
     public static final RegistryObject<SoundEvent> WING_BLOCK_JUMP = registerSoundEvent("wing_block_jump");
+    public static final RegistryObject<SoundEvent> DMC5_VERGIL_THEME = registerSoundEvent("dmc5_vergil_theme");
+    public static final RegistryObject<SoundEvent> DMC5_DANTE_THEME = registerSoundEvent("dmc5_dante_theme");
+    public static final RegistryObject<SoundEvent> DMC3_DEVILS_NEVER_CRY = registerSoundEvent("dmc3_devils_never_cry");
+    public static final RegistryObject<SoundEvent> DMC5_NERO_THEME = registerSoundEvent("dmc5_nero_theme");
+    public static final RegistryObject<SoundEvent> DMC5_V_THEME = registerSoundEvent("dmc5_v_theme");
+
 
     private static RegistryObject<SoundEvent> registerSoundEvent(String name){
         ResourceLocation id = new ResourceLocation(DevilMayCryMod.MOD_ID, name);
